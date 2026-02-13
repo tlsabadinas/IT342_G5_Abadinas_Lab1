49ecbc9e80b753e7337ad7c55789a5e7704c1706
tlsabadinas <treasure.abadinas@gmail.com>
2026-02-14 00:55:47 +0800

    feat(entity): add entity

---
+++ backend/UserAuth/UserAuth/src/main/java/com/citu/lab2/entity/User.java
@@ -0,0 +1,36 @@
+package com.citu.lab2.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
+    private String password;
+
+    // Getters and Setters
+    public Long getId() { return userId; }
+    public void setId(Long id) { this.userId = userId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
