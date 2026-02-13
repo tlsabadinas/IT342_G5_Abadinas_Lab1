b0208257767f906904dfb92ed0cdc16a6f170e16
tlsabadinas <treasure.abadinas@gmail.com>
2026-02-14 00:36:52 +0800

    setup(project): setup spring boot application

---
+++ backend/UserAuth/UserAuth/src/test/java/com/citu/lab2/UserAuth/UserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citu.lab2.UserAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
